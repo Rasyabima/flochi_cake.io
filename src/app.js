4c8b2e77a8e8644b2913b82f052a54a4ec5dc3ed
Rasyab612 <bimarasya15@gmail.com>
2026-04-17 10:34:39 +0700

    Update path gambar dan tambah folder img

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('products', () => ({
 items: [
-    { id: 1, name: 'souffle cake matcha', img: '1.jpg', price: 20000 },
-    { id: 2, name: 'souffle cake chocolate', img: '2.jpg', price: 15000 },
-    { id: 3, name: 'Chocolate Cake', img: '3.jpg', price: 25000 },
+    { id: 1, name: 'souffle cake matcha', img: 'coklat.jpeg', price: 10000 },
+    { id: 2, name: 'souffle cake chocolate', img: 'matcha.jpg', price: 10000 },
+    { id: 3, name: 'souffle cake red velvet', img: 'redvelvet.jpg', price: 10000 },
     { id: 4, name: 'Strawberry Cake', img: '4.jpg', price: 30000 },
     { id: 5, name: 'Cheesecake', img: '5.jpg', price: 35000 },
 ],
